4c8c5a0caad11f4365030d62f6aa98287afbaa03
ChaloupkaM01 <95072894+ChaloupkaM01@users.noreply.github.com>
2025-03-24 11:44:24 +0100

    Update changeip.sh

--- changeip.sh
+++ changeip.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Konfigurace připojení k databázi
-DB_USER="root"          # Změň podle potřeby
-DB_PASS="tvoje_heslo"   # Změň podle potřeby
-DB_NAME="wordpress"     # Název databáze WordPressu
-TABLE_NAME="wp_options" # Název tabulky (změň, pokud máš jiný prefix)
+DB_USER="wpadmin"
+DB_PASS="extrasilneheslo+25"
+DB_NAME="wordpress"
+TABLE_PREFIX="wp01DP_"
 
 # Získání aktuální IP adresy serveru
 CURRENT_IP=$(hostname -I | awk '{print $1}')
@@ -13,14 +13,38 @@ if [[ -z "$CURRENT_IP" ]]; then
     exit 1
 fi
 
-echo "Aktualizuji WordPress URL na IP: $CURRENT_IP"
+echo "Probíhá aktualizace všech IP adres na http://$CURRENT_IP"
 
-# Aktualizace URL v databázi
+# Funkce pro aktualizaci IP adres v tabulce
+update_ip_addresses() {
+    local table=$1
+    local column=$2
+    mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -e "
+        UPDATE $table 
+        SET $column = REGEXP_REPLACE($column, 'http://[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+', 'http://$CURRENT_IP')
+        WHERE $column REGEXP 'http://[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+';
+    "
+}
+
+# Aktualizace v různých tabulkách
 mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -e "
-  UPDATE $TABLE_NAME SET option_value = 'http://$CURRENT_IP' WHERE option_name IN ('siteurl', 'home');
+    -- Aktualizace základních URL
+    UPDATE ${TABLE_PREFIX}options SET option_value = 'http://$CURRENT_IP' WHERE option_name IN ('siteurl', 'home');
+    UPDATE ${TABLE_PREFIX}options SET option_value = 'http://$CURRENT_IP/wp-content/uploads' WHERE option_name = 'upload_url_path';
 "
 
-# Restart Apache pro zajištění správného fungování
-systemctl restart apache2
+# Aktualizace IP adres v různých tabulkách
+update_ip_addresses "${TABLE_PREFIX}posts" "post_content"
+update_ip_addresses "${TABLE_PREFIX}posts" "guid"
+update_ip_addresses "${TABLE_PREFIX}postmeta" "meta_value"
+update_ip_addresses "${TABLE_PREFIX}options" "option_value"
+update_ip_addresses "${TABLE_PREFIX}usermeta" "meta_value"
+update_ip_addresses "${TABLE_PREFIX}comments" "comment_content"
+update_ip_addresses "${TABLE_PREFIX}links" "link_url"
+update_ip_addresses "${TABLE_PREFIX}links" "link_image"
 
-echo "Aktualizace dokončena!"
+echo "Provedena komplexní aktualizace IP adres v databázi"
+
+# Restart služeb
+systemctl restart apache2
+systemctl restart mariadb
